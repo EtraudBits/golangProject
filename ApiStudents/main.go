42f5e0d3e42a2dc0cb5a226c504ac126e27377c9
Duarte <du84arte@gmail.com>
2025-11-26 19:43:47 -0300

    Add Linhas comentadas explicando o código

--- ApiStudents/main.go
+++ ApiStudents/main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
-  "log/slog"
-  "net/http"
-  "errors"
+  "github.com/labstack/echo/v4" // framework web usado para criar rotas e servidor HTTP
+  "github.com/labstack/echo/v4/middleware" //middleware já prontos do Echo
+  "log/slog" // logger estruturado da biblioteca Padrão
+  "net/http" // códigos e funções HTTP da standard lib
+  "errors" // para manipulação de erros
 )
 
 func main() {
   // Echo instance
-  e := echo.New()
+  e := echo.New() //cria uma nova instância da estrutura principal do Echo, que representa o servidor WEB.
 
   // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+  e.Use(middleware.Logger()) //Adiciona um middleware que registra logs de cada requisição HTTP feita no servidor.
+  e.Use(middleware.Recover()) // adicona um middleware que recupera o servidor caso aconteça um panic, evitando que a aplicação caia.
 
-  // Routes // Rotas
+  // Routes // Define rotas HTTP usando métodos
   e.GET("/students", getStudent)
 
-  // Start server // Iniciando o Servidor
+  // Start server // inica o servidor na porta "8080"
   if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
-    slog.Error("failed to start server", "error", err)
+    slog.Error("failed to start server", "error", err) // caso ocorra um erro que não seja o erro padrão de servidor fechado (http.ErrServerClosed), registra o erro usando slog.Error.
   }
 }
 
-// Handler //Funcões para Rotas
-func getStudent(c echo.Context) error {
-  return c.String(http.StatusOK, "List of all students")
+// Handler //Funcões executada quando a rota é chamada
+func getStudent(c echo.Context) error { //recebe um echo.context, que contém informações da requisição e métodos para responder.
+  return c.String(http.StatusOK, "List of all students") // retorna uma resposta HTTP com status 200 (ok)
 }
