4a99745993b2be023f251cb938de9016a7a5b13a
duarteoliveira <du84arte@gmail.com>
2025-12-18 21:50:30 -0300

    inclusão do handler.go (http)

---
+++ gobuild/internal/budget/handler.go
@@ -0,0 +1,48 @@
+package budget
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct {
+	svc *Service
+}
+
+func NewHandler(svc *Service) *Handler {
+	return &Handler{
+		svc: svc,
+	}
+}
+
+func (h *Handler) RegisterRoutes (g *echo.Group) {
+	g.POST("", h.Create)
+}
+
+type CreateBudgetRequest struct {
+	Customer string `json:"customer"`
+	Items []CreateItemRequest `json:"items"`
+}
+
+func (h *Handler) Create(c echo.Context) error {
+
+	var req CreateBudgetRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "JSON invalido",
+		})
+	}
+	budget, err := h.svc.Create(
+		c.Request().Context(),
+		req.Customer,
+		req.Items,
+	)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": err.Error(),
+		})
+	}
+	return c.JSON(http.StatusCreated, budget)
+}
